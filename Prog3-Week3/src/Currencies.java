d9e0c228d5d5146079594c877afb96ce22c56521
Kashif Rasool <kashif.rasool@tuni.fi>
2022-09-14 17:23:42 +0300

    Currency rates exercise solved vF

--- Prog3-Week3/src/Currencies.java
+++ Prog3-Week3/src/Currencies.java
@@ -21,22 +21,29 @@ public class Currencies {
         TreeMap<String, Double> rates = new TreeMap<>();
 
         commandParams.add(input.next());
+
         label:
         while (true) {
 
             switch (commandParams.get(0)) {
+
                 case "rate":
                     String currency = input.next();
                     commandParams.add(currency);
                     String currencyRateStr = input.next();
                     double currencyRate = Double.parseDouble(currencyRateStr);
+
                     commandParams.add(String.valueOf(currencyRate));
+
                     System.out.printf(Locale.US, "%s %s %.10f%n", commandParams.get(0), currency, currencyRate);
                     System.out.println(String.format(Locale.US, "Stored the rate 1 EUR = %.3f %s", currencyRate, currency.toUpperCase()));
+
                     rates.put(currency.toUpperCase(), currencyRate);
                     break;
 
                 case "rates":
+                    System.out.println(commandParams.get(0));
+
                     System.out.println("Stored euro rates:");
                     for (Map.Entry<String, Double> rate : rates.entrySet()) {
                         System.out.printf(Locale.US, "  %s %.3f%n", rate.getKey(), rate.getValue());
@@ -48,17 +55,23 @@ public class Currencies {
                         String amountEntered = input.next();
                         double amount2BConverted = Double.parseDouble(amountEntered);
                         commandParams.set(1, input.next());
+
+                        System.out.printf(Locale.US, "%s %s %s%n", commandParams.get(0), amountEntered, commandParams.get(1));
+
                         if (rates.containsKey(commandParams.get(1))) {
                             System.out.printf(Locale.US, "%.3f %s = %.3f EUR%n", amount2BConverted, commandParams.get(1), amount2BConverted / rates.get(commandParams.get(1)));
                         } else {
                             System.out.println("No rate for " + commandParams.get(1).toUpperCase() + " has been stored!");
                         }
                         break;
+
                     } catch (Exception exception) {
                         exception.printStackTrace();
                     }
 
                 case "quit":
+                    System.out.println(commandParams.get(0));
+
                     System.out.println("Quit-command received, exiting...");
                     break label;
 
